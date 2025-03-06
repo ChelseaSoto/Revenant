5a6062889f07d92496a0277e87e2afe277dad80c
Charvina Viands <ch748292@ucf.edu>
2025-03-05 22:03:00 -0500

    gamma slider

---
+++ Source/Revenant/Public/UOGameUserSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "UOGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REVENANT_API UUOGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+	
+};
